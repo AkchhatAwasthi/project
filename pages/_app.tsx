447d42f29c565cead87a87fa23e0e47963d14f01
Akchhat Awasthi <150040162+AkchhatAwasthi@users.noreply.github.com>
2025-09-07 22:10:11 +0530

    Properly fix Tailwind CSS and SSR issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import type { AppProps } from "next/app";
 import { useState } from "react";
-import dynamic from "next/dynamic";
 import "@/styles/globals.css";
-
-// Dynamic imports for client-side only components
-const Toaster = dynamic(() => import("@/components/ui/toaster").then(mod => ({ default: mod.Toaster })), {
-  ssr: false
-});
-
-const Sonner = dynamic(() => import("@/components/ui/sonner").then(mod => ({ default: mod.Toaster })), {
-  ssr: false
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
@@ -21,8 +11,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Component {...pageProps} />
-        <Toaster />
-        <Sonner />
       </TooltipProvider>
     </QueryClientProvider>
   );
